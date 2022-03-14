89a84c16f89b0a3fcdc89293ce3354ac7201e630
nickrose27 <101585146+nickrose27@users.noreply.github.com>
2022-03-14 10:09:56 -0700

    Add files via upload

---
+++ ProjectNotebook-Copy1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Project Description
+
+# All about the Joke Machine!
+# Hello!!! Welcome to my final project for COGS18 - Introduction to Python! This project features my creation of the Joke Machine! initally the user inputs their name and describes their day, which will be used to welcome the user and intorudce them to the Joke Machine. Once acquainted.... the Joke Machine will then ask for and asses the useres current modd in order recomend a corresponding Level Of Laughter(a category, varying in funniness, containg three jokes), the participant will ultimamtly decide the Level Of Laughter category for the jokes they wish to hear. The Joke Machine will then proovide three jokes pertaing to the category selected and will give the user a chance to guess the answer to each joke before providing it. Once all the jokes have been deliverd the Joke Machine will ask the user for a satisfaction rating regarding the jokes given. If overall satisfaction is low the user will have the option to offer feedback to the Joke Machine. Time to laugh!
+# 
+
+# ## Project Code
+# 
+# If it makes sense for your project, you can have code and outputs here in the notebook as well.
+
+# In[14]:
+
+
+from my_module.functions import start, snicker, laughing_out_loud, rolling_on_floor, satisfaction
+get_ipython().system('pytest')
+
+
+# In[ ]:
+
+
+# Do a bunch of things.
+start()
+
+
+# In[ ]:
+
+
+# test it out
+    
+
+
+# #### Extra Credit (*optional*)
+#  
+# 1. This is the very first time in my life that I have taken a coding class! Coming into COGS18 I had no idea what coding was like or what coding even consisted of. For the longest time I thought that coding was just using zero's and one's to create things like fancy website and algorythms. Little did I know what coding was all about. I would like to say that I am greatful for everything that I've learned in this class because coming into this course I did not think that I was capable or writing a line of code whatsoever.
+# 2. How your project went above and beyond the requirements of the project and/or how you challenged yourself to learn something new with the final project
+# The requirments of the course were to creat three uneaque and origional funcitons/methods that accomplished something that new. I believe that I have done that and then some. Not only does my project containe nearly 200 lnes of code but also has two additional functions in light of the three that were required, contributing to five functions total. While I did not think that I could accmomplish creating the ammount of code that I did for this project, at the begining of the class I still managed to do so. Not only have I surpassed my own expectations but I have also mannaged to encororperate the use of user inputs in my coding project as well as creating functions that play on other functions. This project has been both stressful and fun and now serves as proof that I can code, all doubt aside!!!
