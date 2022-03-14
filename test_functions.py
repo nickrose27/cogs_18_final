23828b974db88701617a53b6cf5b8d2f35e2cb2a
nickrose27 <101585146+nickrose27@users.noreply.github.com>
2022-03-14 09:52:10 -0700

    Create test_functions.py

---
+++ test_functions.py
@@ -0,0 +1,27 @@
+"""Test for my functions.
+
+Note: because because the functions used within my project do not take in inital 
+parameters and dont return anything it was not sure how to creat unit tests other than what is seen below.
+"""
+
+from functions import start, snicker, laughing_out_loud, rolling_on_floor, satisfaction
+
+def test_start():
+    
+    assert callable(start)
+    
+def test_snicker():
+    
+    assert callable(start)
+    
+def test_laughing_out_loud():
+    
+    assert callable(laughing_out_loud)
+    
+def test_rolling_on_floor():
+    
+    assert callable(rolling_on_floor)
+    
+def test_satisfaction():
+    
+    assert callable(satisfaction)
